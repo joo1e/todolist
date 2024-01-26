2d9f2a6a9a2808d924c5a4f22c76db041d35b481
안주원 <urams79@gmail.com>
2024-01-26 11:33:23 +0900

    project_todolist

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/layout/Header";
+
+const App = () => {
+  return (
+  <div>
+    <Headeer />
+  </div>
+  )
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const header = () => {
+    return (
+        <header>
+        <h1>My todo list</h1>
+        <h1>React</h1>
+    </header>
+    )
+  };
+  
+  export default Header;
+  
